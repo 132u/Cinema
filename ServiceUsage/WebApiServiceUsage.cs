1fc13bdc529a7338fea490fd6b5c2ea7dcc3f817
irina <132u@mail.ru>
2018-06-11 13:42:55 +0000

    My first commit ti git

---
+++ ServiceUsage/WebApiServiceUsage.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Web.Configuration;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace ServiceUsage
+{
+    public class WebApiServiceUsage
+    {
+        private static HttpClient  _client;
+
+        public WebApiServiceUsage(string url)
+        {
+            _client = new HttpClient
+            {
+                BaseAddress = new Uri(url)
+            };
+            _client.DefaultRequestHeaders.Accept.Clear();
+            _client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+       
+
+        public async Task<HttpResponseMessage> GetAsync(string path)
+        {
+            var t=  _client.GetAsync(path);
+            return await _client.GetAsync(path);
+        }
+    }
+}
---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.Mvc;
+using ServiceUsage;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private static WebApiServiceUsage _service = new WebApiServiceUsage(WebConfigurationManager.AppSettings["WebApiService"]);
+
+        public async Task<ActionResult> Index()
+        {
+            var response = await _service.GetAsync("api/cinema");
+            var r = response.Content.ReadAsStringAsync().Result;
+            return View(await response.Content.ReadAsStringAsync());
+        }
+
+        //public ActionResult Index()
+        //{
+        //    ViewBag.Title = "Home Page";
+
+        //    return View();
+        //}
+    }
+}
